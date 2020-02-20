8cc67c79c00a94e5055de56f06ffb12d14dace61
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-20 10:34:45 -0700

    seed file

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,5 +1,7 @@
 class SubsController < ApplicationController
+  before_action :set_sub, only: [:show, :edit, :update, :create, :destroy]
   def index
+    @sub = Sub.all
   end
 
   def show
@@ -9,5 +11,10 @@ class SubsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def set_sub
+    @sub = Sub.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'subs#index'
-
+  resources :subs #adds resources for all of our routes
+  
+  # if we don't want to use resouces we can say specifically what we want
+  #   get "/subs", to: "subs#index"
+  #   get "/sub/:id", to "sub#show"
+  #   delete "/sub/:id", to: "sub#destroy"
    end
