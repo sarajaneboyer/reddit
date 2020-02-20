5d28bf658f738835163e84104180994c519adc89
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-20 12:06:53 -0700

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root 'subs#index'
-  resources :subs #adds resources for all of our routes
-  
+  resources :subs do#adds resources for all of our routes
+    resources :topics
+    end
+
+
   # if we don't want to use resouces we can say specifically what we want
   #   get "/subs", to: "subs#index"
   #   get "/sub/:id", to "sub#show"
   #   delete "/sub/:id", to: "sub#destroy"
+
+
+
    end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Sub.create(name:'Javascript')
-Sub.create(name:'HTML')
-Sub.create(name:'CSS')
+require 'faker'
+
+# sub_x = Sub.create(name: 'name')
+# sub_x.topics.create(name: 'name', body: 'body') a way to create one sub with one topic
+
+10.times do 
+  name = Faker::Artist.name
+  sub = Sub.create(name: name)
+  3.times do |j|
+    sub.topics.create(name: "#{name} topic index: #{j}",  body:'body' ) 
+  end
+end
+
